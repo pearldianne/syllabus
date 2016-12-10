0efcb750d74a3051f5855721f814c69ee7bbe04c
pearldianne <cpearldianne@gmail.com>
2016-12-10 08:40:29 +0800

    Update and rename README.md to EngStudent.java

--- EngStudent.java
+++ EngStudent.java
@@ -187,3 +187,5 @@ You'll have to learn this on your own. Please refer to the github guides in the 
 Reuse and building upon ideas or code are major parts of modern software development.  As a programmer you will never (or try not to) write anything from scratch.  This class is structured such that all solutions are public.  You are encouraged to learn from the work of your peers.  I won't hunt down people who are simply copying-and-pasting solutions, because without challenging themselves, they  are simply wasting their time and money taking this class.
 
 Please respect the terms of use and/or license of any code you find, and if you reimplement or duplicate an algorithm or code from elsewhere, credit the original source with an inline comment.
+
+Cunada,Pear Dianne
